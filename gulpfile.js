771ed7c65c0dbea689314d913b41e7ad47dddc95
digamber <digamberpradhan89@gmail.com>
2019-12-02 14:54:33 +0545

    Update for revamp

--- gulpfile.js
+++ gulpfile.js
@@ -15,26 +15,28 @@ To Do List
 		All online blogs says its a good tool but I am not sure right now
 */
 
-let gulp 			= require('gulp');
-let sass 			= require('gulp-sass'); // compiles SASS to CSS
-let sourcemaps 		= require('gulp-sourcemaps'); // generate css source maps
-let notify 		 	= require('gulp-notify'); // provides notification to use once task is complete
-let uglify 			= require('gulp-uglify'); // minifies js files
-let uglifycss    	= require('gulp-uglifycss'); // minifies css files
-let concat       	= require('gulp-concat');  //concatenates multiple js files 
-let rename       	= require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
-let plumber 		= require('gulp-plumber');
-let autoprefixer    = require('gulp-autoprefixer');
+const {src, dest, watch, parallel, series} = require('gulp');
+const sass = require('gulp-sass'); // compiles SASS to CSS
+const sourcemaps = require('gulp-sourcemaps'); // generate css source maps
+const notify = require('gulp-notify'); // provides notification to use once task is complete
+const uglify = require('gulp-uglify'); // minifies js files
+const concat = require('gulp-concat');  //concatenates multiple js files
+const rename = require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
 
-let stylesSource 			 = './assets/css/**/*.scss';
-let jsVendorSource 			 = './assets/js/vendor/*.js';
-let jsVendorDestination      = './js';
-let jsVendorFile 			 = 'vendor';
+let stylesSource = './resources/scss/**/*.scss';
+let stylesDestination = './assets/css';
+let jsVendorSource = './resources/js/vendor/*.js';
+let jsVendorDestination = './js';
+let jsVendorFile = 'vendor';
 
-let jsCustomSource 			 = './assets/js/custom/*.js';
-let jsCustomDestination 	 = './js';
-let jsCustomFile	 		 = 'main';
+let jsCustomSource = './resources/js/custom/*.js';
+let jsCustomDestination = './assets/js';
+let jsCustomFile = 'main';
 
 
 /*
@@ -42,62 +44,69 @@ let jsCustomFile	 		 = 'main';
 	generates sourcemap
 	generates css and put it css folder in route
 */
-gulp.task('compileStyles', function(){
-	return gulp.src(stylesSource)
-			.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}) )
-			.pipe(sourcemaps.init())
-			.pipe(sass())
-			.pipe(autoprefixer({
-				  browsers: ['last 2 versions'],
-            	  cascade: false
-			}))
-			.pipe(sourcemaps.write('./maps'))
-			.pipe(gulp.dest('./css'))
-			.pipe(uglifycss({
-      				"maxLineLen": 80,
-      				"uglyComments": true
-    		}))
-    		.pipe( rename( { suffix: '.min' } ) )
-    		.pipe(gulp.dest('./css'))
-			.pipe( notify( { message: 'TASK: "styles" Completed! 💯', onLast: true } ) );
-});
+function compileMinifiedStyles() {
+    return src(stylesSource)
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['node_modules/bootstrap/scss/']
+        }))
+        .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest(stylesDestination))
+        .pipe(notify({message: 'TASK: "styles" Completed! 💯', onLast: true}));
+}
+
+function compileUnminifiedStyles() {
+    return src(stylesSource)
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['node_modules/bootstrap/scss/']
+        }))
+        .pipe(postcss([autoprefixer]))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(dest(stylesDestination))
+        .pipe(notify({message: 'TASK: "styles" Completed! 💯', onLast: true}));
+}
 
 /*Compile Files in js/vendor intended for vendor scripts example bootstrap, meanmenu, etc*/
-gulp.task('compileVendorJS', function(){
-	return gulp.src(jsVendorSource)
-		   .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}) )
-		   .pipe( concat( jsVendorFile + '.js' )  )
-		   .pipe( gulp.dest( jsVendorDestination ) )
-		   .pipe( rename( {
-       			basename: jsVendorFile,
-       			suffix: '.min'
-    	 	}))
-		   .pipe( uglify() )
-		   .pipe( gulp.dest( jsVendorDestination ) )
-		   .pipe( notify( { message: 'TASK: "compileVendorJS" Completed! 💯', onLast: true } ) );
-});
+function compileVendorJS() {
+    return src(jsVendorSource)
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(concat(jsVendorFile + '.js'))
+        .pipe(dest(jsVendorDestination))
+        .pipe(rename({
+            basename: jsVendorFile,
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(dest(jsVendorDestination))
+        .pipe(notify({message: 'TASK: "compileVendorJS" Completed! 💯', onLast: true}));
+}
 
 
 /*Compile Files in Custom JS intended for non-vendor scripts*/
-gulp.task('compileCustomJS', function(){
-	return gulp.src(jsCustomSource)
-		   .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}) )
-		   .pipe( concat( jsCustomFile  + '.js' )  )
-		   .pipe( gulp.dest( jsCustomDestination ) )
-		   .pipe( rename( {
-       			basename: jsCustomFile,
-       			suffix: '.min'
-    	 	}))
-		   .pipe( uglify() )
-		   .pipe( gulp.dest( jsCustomDestination ) )
-		   .pipe( notify( { message: 'TASK: "compileCustomJS" Completed! 💯', onLast: true } ) );
+function compileCustomJS() {
+    return src(jsCustomSource)
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(concat(jsCustomFile + '.js'))
+        .pipe(dest(jsCustomDestination))
+        .pipe(rename({
+            basename: jsCustomFile,
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(dest(jsCustomDestination))
+        .pipe(notify({message: 'TASK: "compileCustomJS" Completed! 💯', onLast: true}));
+}
+
+exports.compileVendorJS = compileVendorJS;
+exports.compileCustomJS = compileCustomJS;
+    exports.default = parallel(compileUnminifiedStyles, compileMinifiedStyles, compileVendorJS, compileCustomJS, (done) => {
+    watch(stylesSource, parallel(compileUnminifiedStyles, compileUnminifiedStyles));
+    watch(jsVendorSource, compileVendorJS);
+    watch(jsCustomSource, compileCustomJS);
+    done();
 });
-
-
-/*Default tasks that will be run when using "gulp" command*/
-gulp.task('default', gulp.parallel('compileStyles', 'compileVendorJS', 'compileCustomJS',  (done) => {
-	gulp.watch( stylesSource, gulp.series('compileStyles'));
-	gulp.watch( jsVendorSource, gulp.series('compileVendorJS'));
-	gulp.watch( jsCustomSource, gulp.series('compileCustomJS'));
-	done();
-}));
